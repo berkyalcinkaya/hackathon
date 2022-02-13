3e1387dcdf5966af7c0c034df6442708f51ba626
Leo Gagnon <leognon@gmail.com>
2022-02-13 11:51:43 -0500

    Fixed routes

--- app.py
+++ app.py
@@ -3,13 +3,21 @@ from flask import Flask, render_template, send_from_directory
 
 app = Flask(__name__, static_url_path='/templates')
 
-@app.route('/templates/<path:path>')
-def index(path):
-    return send_from_directory('templates', path)
-
 @app.route("/")
-def start():
+def index():
     return send_from_directory("templates", "index.html")
+
+@app.route("/main.css")
+def mainCSS():
+    return send_from_directory("templates", "main.css")
+
+@app.route("/presetCourses.js")
+def presetCoursesJS():
+    return send_from_directory("templates", "presetCourses.js")
+
+@app.route("/script.js")
+def scriptJS():
+    return send_from_directory("templates", "script.js")
 
 if __name__ == '__main__':
     # run app in debug mode on port 5000
