7427f0d0bb618fc12f45ffd456622ad6e14eac4b
Berk Yalcinkaya <berkyalcinkaya55@gmail.com>
2022-02-13 11:40:34 -0500

    flask debugging

--- app.py
+++ app.py
@@ -1,11 +1,15 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, send_from_directory
 
-app = Flask(__name__)
+
+app = Flask(__name__, static_url_path='/templates')
+
+@app.route('/templates/<path:path>')
+def index(path):
+    return send_from_directory('templates', path)
 
 @app.route("/")
-def index():
-    return render_template("index.html")
-
+def start():
+    return send_from_directory("templates", "index.html")
 
 if __name__ == '__main__':
     # run app in debug mode on port 5000
